27c02750f210a90207377defe3dc22d5ce20aaee
David Hartmann <david.hartmann@edu.dualis-institut.de>
2026-05-12 10:19:17 +0200

    Modul Style_Config hinzugefügt

---
+++ infrastructure/style_config.py
@@ -0,0 +1,12 @@
+
+# style_config.py
+
+GOLD    = "\033[33m" 
+GELB    = "\033[33m"
+SILBER  = "\033[37m" 
+BRONZE  = "\033[31m" 
+CYAN    = "\033[36m" 
+GRUEN   = "\033[32m" 
+ROT     = "\033[31m" 
+RESET   = "\033[0m"  
+FETT    = "\033[1m"
